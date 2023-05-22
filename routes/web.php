ff4ec4e604b66536a516379e74830216a00c55b4
Nam <55947650+ThisisNam868@users.noreply.github.com>
2023-05-22 05:45:56 +0000

    First installation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('chirps', ChirpController::class)
+->only(['index', 'store'])
+->middleware(['auth', 'verified']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
